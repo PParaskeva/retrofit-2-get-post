4f563c823955c6031a4a6dedade6280b5ebceef8
PParaskeva <pnagiotis@yahoo.gr>
2017-01-25 10:19:29 +0000

    creation of the project

---
+++ application/MyApplication/app/src/main/java/com/example/panagiotis/retrofit2_post_get/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.panagiotis.retrofit2_post_get;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
